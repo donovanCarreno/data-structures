ae1daa268e22592577ae858f86a14817bb95a316
donovanCarreno <donovan.carreno@gmail.com>
2016-06-08 15:09:53 -0700

    Function-Queue completed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,23 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  var key = 0;
+  var oldest = 0;
 
   someInstance.enqueue = function(value) {
+    storage[key] = value;
+    key++;
   };
 
   someInstance.dequeue = function() {
+    var store = storage[oldest];
+    delete storage[oldest];
+    oldest++;
+    return store;
   };
 
   someInstance.size = function() {
+    return key - oldest < 0 ? 0 : key - oldest;
   };
 
   return someInstance;
