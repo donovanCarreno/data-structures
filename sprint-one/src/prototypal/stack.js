af22d179877d2c07385dde60b5de65a89e3d7978
donovanCarreno <donovan.carreno@gmail.com>
2016-06-08 16:42:12 -0700

    Need few more specs to pass for Prototypical Stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = Object.create(Stack.prototype);
+
+  newInstance.key = 0;
+  newInstance.storage = {};
+
+  return newInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.key] = value;
+  this.key++;
+};
 
+stackMethods.pop = function() {
+  if(this.key > 0){
+    var store = this.storage[this.key - 1];
+    delete this.storage[this.key - 1];
+    this.key--;
+    return store;
+  }
+};
+
+stackMethods.size = function() {
+  return this.key;
+};
+
+Stack.prototype.push = stackMethods.push;
+
+Stack.prototype.pop = stackMethods.pop;
+
+Stack.prototype.size = stackMethods.size;
