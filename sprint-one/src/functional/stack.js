e27819d50d43e7f7416f28c9e0c3ca1a93a38a0e
Omneet Lachhar <omneetlachhar@gmail.com>
2016-06-08 12:24:37 -0700

    Passing spec for functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,14 +5,31 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
+  var keys = 0;
   someInstance.push = function(value) {
+    storage[keys] = value;
+    keys++;
   };
 
   someInstance.pop = function() {
+    if(keys > 0){
+      var store = storage[keys - 1];
+      delete storage[keys];
+      keys--;
+    }
+    return store;
   };
 
   someInstance.size = function() {
+      return keys;
   };
 
   return someInstance;
 };
+
+// var myStack = stack();
+// myStack.push(5);
+// myStack.push('oranges');
+// myStack.size() //2
+// myStack.pop() // 'oranges'
+// myStack.size() // 1
