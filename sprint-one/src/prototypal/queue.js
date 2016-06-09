ef6043faa5a2f32d5e12244620be8dd9fb08428d
donovanCarreno <donovan.carreno@gmail.com>
2016-06-08 20:06:48 -0700

    Queue-prototype passing all specs

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = Object.create(queueMethods);
+  newInstance.key = 0;
+  newInstance.oldest = 0;
+  newInstance.storage = {};
+
+  return newInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.key] = value;
+  this.key++;
+};
+
+queueMethods.dequeue = function() {
+  if (this.key > this.oldest) {
+    var store = this.storage[this.oldest];
+    delete this.storage[this.oldest];
+    this.oldest++;
+    return store;
+  }
+};
+
+queueMethods.size = function() {
+  return this.key - this.oldest;
+};
+
+
+
 
 
