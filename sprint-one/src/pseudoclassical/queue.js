bc832c698e795950849f54b9f3a7d4501dbfc309
donovanCarreno <donovan.carreno@gmail.com>
2016-06-08 20:31:36 -0700

    Psuedo-classical queue passing all Specs

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.key = 0;
+  this.oldest = 0;
+  this.storage = {};
+
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.key] = value;
+  this.key++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.key > this.oldest) {
+    var store = this.storage[this.oldest];
+    delete this.storage[this.oldest];
+    this.oldest++;
+    return store;
+  }
+};
+
+Queue.prototype.size = function() { 
+  return this.key - this.oldest;
+};
 
