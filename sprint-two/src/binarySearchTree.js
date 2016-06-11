cbeab94602bf4de80f86e609b278cf0c3c7623d3
donovanCarreno <donovan.carreno@gmail.com>
2016-06-10 20:12:19 -0700

    binarySearchTree pass all specs

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,53 @@
 var BinarySearchTree = function(value) {
+  var newTree = {};
+  newTree.value = value;
+  newTree.left;
+  newTree.right;
+
+  _.extend(newTree, binaryMethods);
+
+  return newTree;
 };
 
+var binaryMethods = {};
+
+binaryMethods.insert = function(value, node) {
+  if (value > this.value) {
+    if (this.right === undefined) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  } else {
+    if (this.left === undefined) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+};
+
+binaryMethods.contains = function(target) {
+  if (target === this.value) {
+    return true;
+  } else if (this.right !== undefined) {
+    return this.right.contains(target);
+  } else if (this.left !== undefined) {
+    return this.left.contains(target);
+  } else {
+    return false;
+  }
+};
+
+binaryMethods.depthFirstLog = function(callback) {
+  callback(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(callback);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
